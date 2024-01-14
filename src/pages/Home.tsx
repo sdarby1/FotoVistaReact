4c6881af16c1cb715021dc6254c6b88cf968ed04
sdarby1 <siensienderby@gmail.com>
2024-01-14 14:25:24 +0100

    login form

---
+++ src/pages/Home.tsx
@@ -0,0 +1,7 @@
+const Home = () => {
+  return (
+    <h1>Startseite</h1>
+  )
+}
+
+export default Home
---
+++ src/pages/Register.tsx
@@ -0,0 +1,16 @@
+import { useForm } from "react-hook-form"
+import { DevTool } from "@hookform/devtools"
+
+type FormValues = {
+    username: string,
+    email: string,
+    password: string,
+}
+
+const Register = () => {
+  return (
+    <div>Register</div>
+  )
+}
+
+export default Register
